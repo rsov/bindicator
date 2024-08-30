defd44bb8fe5401aa63a4623ddfd5ca16e3804b4
rsov <ruzzzlan@hotmail.com>
2024-08-30 11:11:12 +0000

    Fix rain, add UV and other graph nice things

--- src/components/weather.rs
+++ src/components/weather.rs
@@ -1,9 +1,10 @@
 use charming::{
-    component::{Axis, Grid},
+    component::{Axis, Grid, Legend},
     element::{
-        AxisTick, AxisType, ItemStyle, LineStyle, MarkArea, MarkAreaData, SplitLine, Symbol,
+        AxisLabel, AxisTick, AxisType, ItemStyle, LabelAlign, LineStyle, MarkArea, MarkAreaData,
+        SplitLine, Symbol, TextStyle, Tooltip, Trigger,
     },
-    series::Line,
+    series::{Align, Line},
     Chart, WasmRenderer,
 };
 use chrono::{DateTime, Local};
@@ -44,6 +45,7 @@ struct WeatherHourly {
     temperature_2m: Vec<f32>,
     precipitation: Vec<f32>,
     time: Vec<String>,
+    uv_index: Vec<f32>,
 }
 
 #[derive(Debug, Clone, PartialEq, Deserialize, Default)]
@@ -92,7 +94,10 @@ pub fn WeatherComponent() -> Html {
                     ["latitude", &data.latitude.to_string()],
                     ["longitude", &data.longitude.to_string()],
                     ["timezone", &"auto".to_string()],
-                    ["hourly", &["temperature_2m", "precipitation"].join(",")],
+                    [
+                        "hourly",
+                        &["temperature_2m", "precipitation", "uv_index"].join(","),
+                    ],
                     [
                         "daily",
                         &[
@@ -181,6 +186,7 @@ fn HourlyComponent(props: &HourlyComponentProps) -> Html {
     let mut time = Vec::new();
     let mut temp = Vec::new();
     let mut rain = Vec::new();
+    let mut uv: Vec<f32> = Vec::new();
 
     let offset_hours = props.offset_hours.clone();
 
@@ -196,30 +202,49 @@ fn HourlyComponent(props: &HourlyComponentProps) -> Html {
         if date.is_ok() && date.unwrap() >= current_time {
             time.push(format!("{}", date.unwrap().format("%H:%M")));
             temp.push(props.data.temperature_2m[i]);
-            if props.data.precipitation[i] > 0.0 {
-                rain.push(props.data.precipitation[i]);
-            }
+            rain.push(props.data.precipitation[i]);
+            uv.push(props.data.uv_index[i]);
         } else {
         }
     }
 
     let f = use_async::<_, _, ()>({
         let chart = Chart::new()
+            .legend(
+                Legend::new()
+                    .data(vec!["Temperature", "Precipitation", "UV"])
+                    .text_style(TextStyle::new().color("white")),
+            )
             .x_axis(
                 Axis::new()
                     .type_(AxisType::Category)
                     .data(time.clone())
-                    .axis_tick(AxisTick::new().show(false)),
+                    .axis_tick(AxisTick::new().show(false))
+                    .axis_label(AxisLabel::new().color("white")),
             )
             .y_axis(
+                // Temp Lines
                 Axis::new()
                     .type_(AxisType::Value)
+                    .axis_label(AxisLabel::new().color("white"))
+                    // Doesn't work https://github.com/yuankunzhang/charming/pull/67
+                    // .axis_label(AxisLabel::new().formatter("{value} °C"))
                     .split_line(SplitLine::new().line_style(LineStyle::new().color("grey"))),
+            )
+            .y_axis(
+                // Lines
+                Axis::new()
+                    .type_(AxisType::Value)
+                    .axis_label(AxisLabel::new().color("orange"))
+                    .split_line(SplitLine::new().line_style(LineStyle::new().opacity(0)))
+                    .max(11),
             )
             .series(
                 Line::new()
+                    .name("Temperature")
                     .data(temp.clone())
-                    .symbol(Symbol::None)
+                    .show_symbol(false)
+                    .item_style(ItemStyle::new().color("white"))
                     .line_style(LineStyle::new().width(5).color("white"))
                     .mark_area(
                         MarkArea::new()
@@ -232,13 +257,24 @@ fn HourlyComponent(props: &HourlyComponentProps) -> Html {
             )
             .series(
                 Line::new()
+                    .name("Precipitation")
                     .data(rain.clone())
-                    .symbol(Symbol::None)
+                    .show_symbol(false)
+                    .item_style(ItemStyle::new().color("blue"))
                     .line_style(LineStyle::new().width(3).color("blue")),
             )
-            .grid(Grid::new().top(7).left(25).right(24).bottom(20));
+            .series(
+                Line::new()
+                    .name("UV")
+                    .data(uv.clone())
+                    .y_axis_index(1)
+                    .show_symbol(false)
+                    .item_style(ItemStyle::new().color("orange"))
+                    .line_style(LineStyle::new().width(3).color("orange")),
+            )
+            .grid(Grid::new().top(24).left(24).right(24).bottom(20));
 
-        let renderer = WasmRenderer::new(800, 120);
+        let renderer = WasmRenderer::new(780, 170);
 
         async move {
             renderer.render("chart", &chart).unwrap();
