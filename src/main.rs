a707152f2003ea9782d9d6d7a47d2fd8b8767186
rsov <ruzzzlan@hotmail.com>
2024-10-12 07:36:51 +0000

    Use meta tag to reload the page because it's more reliable in case the code panics

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
 mod components;
-use chrono::{DateTime, Local, NaiveTime};
 use components::bin::BinComponent;
 use components::clock::ClockComponent;
 use components::dim::DimComponent;
 use components::weather::WeatherComponent;
-use web_sys::window;
 
 use yew::{function_component, html, Html};
-use yew_hooks::use_timeout;
 
 #[function_component]
 pub fn App() -> Html {
-    let current: DateTime<Local> = Local::now();
-    let tomorrow = current
-        .checked_add_days(chrono::Days::new(1))
-        .unwrap()
-        .date_naive();
-
-    let midnight = NaiveTime::from_hms_opt(0, 0, 0).unwrap();
-    let midnight = tomorrow
-        .and_time(midnight)
-        .and_local_timezone(Local)
-        .unwrap();
-
-    // Reload the whole page at midnight so it can apply new code from github (if any)
-    use_timeout(
-        || {
-            if let Some(window) = window() {
-                window.location().reload().unwrap();
-            }
-        },
-        midnight.signed_duration_since(current).num_milliseconds() as _,
-    );
-
     html! {
             <div id="app" class="d-flex flex-column justify-content-between p-2" style="overflow: hidden;">
                 <DimComponent/>
