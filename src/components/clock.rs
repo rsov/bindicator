20303a4aea7132c2602e15e65f323be8dc071704
rsov <ruzzzlan@hotmail.com>
2025-01-08 11:41:40 +0000

    Refactor clock into function component

--- src/components/clock.rs
+++ src/components/clock.rs
@@ -1,46 +1,26 @@
-use chrono::{DateTime, Local};
-use futures::StreamExt;
-use std::time::Duration;
-use yew::platform::time::interval;
-use yew::{html, Component, Context, Html};
+use chrono::Local;
+use yew::{function_component, html, use_state, Html};
+use yew_hooks::use_interval;
 
-pub struct ClockComponent {
-    current_time: DateTime<Local>,
-}
+#[function_component]
+pub fn ClockComponent() -> Html {
+    let current_time = use_state(|| Local::now());
 
-pub enum Msg {
-    ClockTicked(DateTime<Local>),
-}
-
-impl Component for ClockComponent {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(ctx: &Context<Self>) -> Self {
-        let time_steam = interval(Duration::from_secs(1)).map(|_| Local::now());
-        ctx.link().send_stream(time_steam.map(Msg::ClockTicked));
-
-        Self {
-            current_time: Local::now(),
-        }
+    {
+        let state = current_time.clone();
+        use_interval(
+            move || {
+                state.set(Local::now());
+            },
+            500,
+        );
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::ClockTicked(current_time) => {
-                self.current_time = current_time;
-            }
-        }
-        true
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <div class="fs-1 text-end fw-bold text-white">
-                { format!("{}", self.current_time.format("%d %b %Y")) }
-                <br/>
-                { format!("{}", self.current_time.format("%H : %M : %S")) }
-            </div>
-        }
+    html! {
+        <div class="fs-1 text-end fw-bold text-white">
+            { format!("{}", current_time.format("%d %b %Y")) }
+            <br/>
+            { format!("{}", current_time.format("%H : %M : %S")) }
+        </div>
     }
 }
